e0b9f4c39f588f3a3ee3fbca670a615a4fa0a575
YeDemirkiran <yedemirkiran27@gmail.com>
2026-03-16 18:20:45 +0300

    Shorten philosopher_eat

--- source/philo/src/philosophers/philosophers_routine.c
+++ source/philo/src/philosophers/philosophers_routine.c
@@ -6,7 +6,7 @@
 /*   By: yademirk <yademirk@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 23:35:34 by yademirk          #+#    #+#             */
-/*   Updated: 2026/03/16 17:59:23 by yademirk         ###   ########.fr       */
+/*   Updated: 2026/03/16 18:13:38 by yademirk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,50 +115,52 @@ static t_byte	take_forks(t_philosopher *philo)
 }
 
 /**
- * @brief Acquires the forks automatically and starts eating.
+ * @brief Updates the last_meal_time property of a philosopher.
  *
- * Checks the death and dinner state before each important state
- * and self-terminates accordingly. Returns 0 on such cases, which means
- * the philosopher should stop and exit immediately.
- *
- * @return 0 on failure (death, dinner over), 1 on success.
+ * @return The last meal time on success, -1 on error.
  */
-t_byte	philosopher_eat(t_philosopher *philo)
+static long	update_last_meal_time(t_philosopher *philo)
 {
 	long	last_meal_time;
-	size_t	new_eat_count;
-	size_t	max_eat_count;
 
-	if (philo == NULL || !take_forks(philo))
-		return (0);
 	last_meal_time = get_time();
 	if (last_meal_time == -1)
 	{
 		philo_error("internal: Error during last_meal_time update");
-		return (0);
+		return (-1);
 	}
 	if (pthread_mutex_lock(&philo->meal_mutex) != SUCCESS)
 	{
 		philo_error("internal: Can't lock meal_mutex during eating");
-		return (0);
+		return (-1);
 	}
 	philo->last_meal_time = last_meal_time;
 	if (pthread_mutex_unlock(&philo->meal_mutex) != SUCCESS)
 	{
 		philo_error("internal: Can't unlock meal_mutex during eating");
-		return (0);
+		return (-1);
 	}
 	if (!should_philo_continue(philo))
 	{
 		leave_forks(philo);
-		return (0);
+		return (-1);
 	}
-	if (philo_message(philo->id, EAT_MESSAGE, last_meal_time) == -1)
-		return (0);
-	if (interval_sleep(philo->config->eat_time, philo) != 1)
-		return (0);
-	if (leave_forks(philo) != 1)
-		return (0);
+	return (last_meal_time);
+}
+
+/**
+ * @brief Increases the eat count property of the philosopher.
+ *
+ * It also checks if the philosopher has eaten enough and returns 0 in such
+ * case.
+ *
+ * @return 0 on either failure or dinner over, 1 on success.
+ */
+static t_byte	increase_eat_count(t_philosopher *philo)
+{
+	size_t	new_eat_count;
+	size_t	max_eat_count;
+
 	if (pthread_mutex_lock(&philo->meal_mutex) != SUCCESS)
 	{
 		philo_error("internal: Can't lock meal_mutex during eating");
@@ -173,6 +175,35 @@ t_byte	philosopher_eat(t_philosopher *philo)
 		return (0);
 	}
 	if (max_eat_count != 0 && new_eat_count >= max_eat_count)
+		return (0);
+	return (1);
+}
+
+/**
+ * @brief Acquires the forks automatically and starts eating.
+ *
+ * Checks the death and dinner state before each important state
+ * and self-terminates accordingly. Returns 0 on such cases, which means
+ * the philosopher should stop and exit immediately.
+ *
+ * @return 0 on failure (death, dinner over), 1 on success.
+ */
+t_byte	philosopher_eat(t_philosopher *philo)
+{
+	long	last_meal_time;
+
+	if (philo == NULL || !take_forks(philo))
+		return (0);
+	last_meal_time = update_last_meal_time(philo);
+	if (last_meal_time < 0)
+		return (0);
+	if (philo_message(philo->id, EAT_MESSAGE, last_meal_time) == -1)
+		return (0);
+	if (interval_sleep(philo->config->eat_time, philo) != 1)
+		return (0);
+	if (leave_forks(philo) != 1)
+		return (0);
+	if (increase_eat_count(philo) != 1)
 		return (0);
 	return (1);
 }
